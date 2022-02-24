3be860dc96f11dce696b831ab5cdea7a456d3896
Ross Fisher <zorrobyte@gmail.com>
2022-02-24 17:01:50 -0500

    PID

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -198,10 +198,10 @@ class CarInterface(CarInterfaceBase):
     ret.steerRateCost = 1.
     ret.centerToFront = ret.wheelbase * 0.44
 
-    if(candidate in TSS2_CAR):
-      ret.steerRateCost = 0.1
-      ret.steerActuatorDelay = 0.25
-      ret.steerControlType = car.CarParams.SteerControlType.angle
+    #if(candidate in TSS2_CAR):
+    #  ret.steerRateCost = 0.1
+    #  ret.steerActuatorDelay = 0.25
+    #  ret.steerControlType = car.CarParams.SteerControlType.angle
 
     # TODO: get actual value, for now starting with reasonable value for
     # civic and scaling by mass and wheelbase
