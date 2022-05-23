7798ece66d9985281d74fffa49697d70eadfadf6
PinoySouljah <100481894+PinoySouljah@users.noreply.github.com>
2022-05-23 13:14:40 -0400

    NoOffroadfix2

--- selfdrive/manager/manager.py
+++ selfdrive/manager/manager.py
@@ -40,6 +40,7 @@ def manager_init() -> None:
     ("CompletedTrainingVersion", "0"),
     ("DisengageOnAccelerator", "1"),
     ("HasAcceptedTerms", "0"),
+    ("NoOffroadFix", "0"),
     ("OpenpilotEnabledToggle", "1"),
   ]
   if not PC:
